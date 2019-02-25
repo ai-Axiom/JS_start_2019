0c1cae3f8128665edf77c6c80c26378b042b5eb4
ai-Axiom <hydra.logos@gmail.com>
2019-02-25 16:45:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ crossorigin="anonymous">
 
 <script src="http://chancejs.com/chance.min.js"></script>
 $ npm install -g live-server
+// Step 6
+function fetchIssues () {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesList = document.getElementById('issuesList');
+  
+  issuesList.innerHTML = '';
+  
+  for (var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var severity = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+    
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                              '</div>';
+  }
+}
